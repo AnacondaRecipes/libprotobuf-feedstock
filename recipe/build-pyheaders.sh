8fd923c177d3730bcb5875ad37c702a6d88151ed
Charles Bousseau <cbousseau@anaconda.com>
2024-05-13 16:39:20 -0400

    adapt recipe

--- recipe/build-pyheaders.sh
+++ recipe/build-pyheaders.sh
@@ -2,4 +2,8 @@
 
 set -exuo pipefail
 
-rsync -a --prune-empty-dirs --include '*/' --include '*.h' --exclude '*' python/ $PREFIX/include/
+# rsync -a --prune-empty-dirs --include '*/' --include '*.h' --exclude '*' python/ $PREFIX/include/
+cd python/
+find . -type d -exec mkdir -p $PREFIX/include/{} \;
+find . -type f -name '*.h' -exec cp {} $PREFIX/include/{} \;
+find $PREFIX/include/ -type d -empty -delete
